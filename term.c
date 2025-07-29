5df8d1c69fea99a7644e2fb84c9309da497a22a9
Emily T. Choe <echoephile@gmail.com>
2025-07-29 09:10:39 -0400

    Go back to no include dir

--- term.c
+++ term.c
@@ -1,13 +1,13 @@
 #define GLAD_GLES2_USE_SYSTEM_EGL
 #define GLAD_GLES2_IMPLEMENTATION
-#include <gles2.h>
+#include "gles2.h"
 
 #include <GLFW/glfw3.h>
 
 #include <stdbool.h>
 #include <stdio.h>
 
-#include <term.h>
+#include "term.h"
 
 int main(int argc, char** argv) {
     int result = 0;
